adc5396a0353f4180074eeb61aa6fe3d8cf19488
swizkon <swizkon@gmail.com>
2024-01-12 13:10:33 +0100

    add some fancy text

--- frontend-svelte-kit-vite/src/components/StoreZone.svelte
+++ frontend-svelte-kit-vite/src/components/StoreZone.svelte
@@ -1,5 +1,7 @@
 <script>
 	import * as THREE from 'three';
+	import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+	import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 	import * as SC from 'svelte-cubed';
 
 	export let position = [0, 0, 0];
@@ -7,6 +9,38 @@
 
 	export let color = 0x003eff;
 	export let visitors = 10;
+
+	let defaultFont;
+	let textGeometry;
+
+	$: visitors > -888 && myfunc();
+
+	function myfunc() {
+		textGeometry = new TextGeometry(`${visitors}`, {
+			font: defaultFont,
+			size: 36,
+			height: 12,
+			curveSegments: 12,
+			bevelThickness: 1,
+			bevelSize: 1,
+			bevelEnabled: false
+		});
+	}
+
+	let loader = new FontLoader();
+	loader.load('/fonts/Permanent_Marker_Regular.json', function (font) {
+		defaultFont = font;
+		textGeometry = new TextGeometry(`${visitors}`, {
+			font: font,
+			size: 36,
+			height: 12,
+			curveSegments: 12,
+			bevelThickness: 1,
+			bevelSize: 1,
+			bevelEnabled: false
+		});
+	});
+
 </script>
 
 <SC.Group scale={[0.1, 0.1, 0.1]} {position}>
@@ -17,4 +51,13 @@
 		position={[1, visitors * 0.5, 0]}
 		castShadow
 	/>
+	{#if textGeometry}
+	<SC.Mesh
+		geometry={textGeometry}
+		material={new THREE.MeshStandardMaterial({ color: color })}
+		scale={[1, 1, 1]}
+		position={[1, visitors, 0]}
+		castShadow
+	/>
+	{/if}
 </SC.Group>
