4903023547398797f17ed83a718f7bb945f01a75
swizkon <swizkon@gmail.com>
2024-01-17 15:06:08 +0100

    extend meta data

--- ES.Labs.RetailRhythmRadar/Configuration/MetadataEnricher.cs
+++ ES.Labs.RetailRhythmRadar/Configuration/MetadataEnricher.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using EventSourcing;
 
 namespace RetailRhythmRadar.Configuration;
@@ -7,6 +8,15 @@ public class MetadataEnricher : IEnrichMetaData
     public IDictionary<string, string> Enrich(IDictionary<string, string> metadata)
     {
         metadata.Add("GitVersion", VersionInfo.GitVersion);
+        metadata.Add("MachineName", Environment.MachineName);
+        metadata.Add("OSVersion", Environment.OSVersion.VersionString);
+        metadata.Add("Version", Environment.Version.ToString());
+        
+        metadata.Add("FrameworkDescription", RuntimeInformation.FrameworkDescription);
+        metadata.Add("OSDescription", RuntimeInformation.OSDescription);
+        metadata.Add("OSArchitecture", RuntimeInformation.OSArchitecture.ToString());
+        metadata.Add("ProcessArchitecture", RuntimeInformation.ProcessArchitecture.ToString());
+        
         return metadata;
     }
 }
